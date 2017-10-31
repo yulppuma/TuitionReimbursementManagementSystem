3532472952ac5e53a540b11cdbac7e6aabb802a5
yulppuma <yulppuma@gmail.com>
2017-10-31 10:42:43 -0400

    doGet servlet works

---
+++ TRMS/TRMS/src/main/java/com/revature/servlet/LoginServlet.java
@@ -0,0 +1,38 @@
+package com.revature.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        System.out.println(username + " " + password);
+         //Need to create the service
+        //String message = service.doLogin(username, password);
+        //response.getWriter().write(message);
+        response.getWriter().write(username + " " + password);
+	}
+
+}
--- TRMS/TRMS/src/main/webapp/js/login.js
+++ TRMS/TRMS/src/main/webapp/js/login.js
@@ -3,20 +3,25 @@
  */
 
 function login(){
+		event.preventDefault();
 		var username = $("#username").val();
 		var password = $("#password").val();
+		//console.log(username + " " + password);
 		if(username == ""){
 			//Whenever form is submitted, the doc is refreshed
-			event.preventDefault();
             $('#messageDiv').text("Enter username");
         }
         if(password == ""){
             $('#messageDiv').append("Enter password");
         }
+        $.ajax({
+        	type: "GET",
+        	url: "LoginServlet",
+        	data: {username: username,password: password},
+        	success : function(results){
+        		console.log(results);
+        	}
+        });
 	}
-
-function init(){
 	//$ is the syntax for jQuery, think of it as a substitute for the document keyword
 	$("#loginForm").submit(login);
-}
-$(document).ready(init);
