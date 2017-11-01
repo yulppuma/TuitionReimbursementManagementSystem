4e5fb6e3ae3ea190859365e76b43f7c8c5d7b50d
spegram91 <spegram91@gmail.com>
2017-11-01 16:29:49 -0400

    minor changes to userDAOImpl

--- TRMS/TRMS/src/main/java/com/revature/trms/dao/UserDAOImpl.java
+++ TRMS/TRMS/src/main/java/com/revature/trms/dao/UserDAOImpl.java
@@ -1,5 +1,30 @@
 package com.revature.trms.dao;
 
-public class UserDAOImpl {
+
+public class UserDAOImpl implements UserDAO {
+
+	
+	
+	public User getUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean insertUser() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean updateUser() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean deleteUser() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
 
 }
